c6b0a42a8b0476a534d7404b46634f96d5a050c2
Hamza Sheikh <hsheikhm@gmail.com>
2015-12-16 14:43:21 +0000

    created a table view controller

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Hamza Sheikh on 16/12/2015.
+//  Copyright © 2015 Hamza Sheikh. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
