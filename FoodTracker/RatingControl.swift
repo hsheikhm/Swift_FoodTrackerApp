bf185ab158b28287bb28c77a9572fb202aa5f9bb
Hamza Sheikh <hsheikhm@gmail.com>
2015-12-16 11:01:19 +0000

    added 5 rating buttons

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -3,17 +3,35 @@ import UIKit
 
 class RatingControl: UIView {
     
+    // MARK: Properties
+    
+    var rating = 0
+    var ratingButtons = [UIButton]()
+    
     // MARK: Initialization
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
-        button.backgroundColor = UIColor.blueColor()
-        button.addTarget(self, action: "ratingButtonTapped:", forControlEvents:
-            .TouchDown)
+        for _ in 0..<5 {
+            let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+            button.backgroundColor = UIColor.blueColor()
+            button.addTarget(self, action: "ratingButtonTapped:", forControlEvents:
+                .TouchDown)
+            ratingButtons += [button]
+            addSubview(button)
+        }
+    }
     
-        addSubview(button)
+    override func layoutSubviews() {
+        var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        
+        // Offset each button's origin by the length of the button plus spacing
+        
+        for (index, button) in ratingButtons.enumerate() {
+            buttonFrame.origin.x = CGFloat(index * (44 + 5))
+            button.frame = buttonFrame
+        }
     }
     
     override func intrinsicContentSize() -> CGSize {
